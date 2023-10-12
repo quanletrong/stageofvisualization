d4e934ba6938f7d58e5fb93a834f90871d45c829
Le Quan <quanletrong@admicro.vn>
2023-10-12 10:10:29 +0700

    be

--- app/appv3.1.4/models/order/Order_model.php
+++ app/appv3.1.4/models/order/Order_model.php
@@ -13,7 +13,7 @@ class Order_model extends CI_Model
         $new_id = 0;
         $ed_type = ED_CTV;
         $iconn = $this->db->conn_id;
-        $sql = "INSERT INTO tbl_order (id_style, create_time, id_user, coupon, pending_pay, status, order_type, create_id_user, ed_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $sql = "INSERT INTO tbl_order (id_style, create_time, id_user, pending_pay, status, order_type, create_id_user, ed_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
         $stmt = $iconn->prepare($sql);
         if ($stmt) {
             $param = [$id_style, $create_time, $id_user, $pending_pay, $status, $order_type, $create_id_user, $ed_type];
