1b2604be22d993eca2111ebdf760a07b89cf6334
Le Quan <quanletrong@admicro.vn>
2024-02-29 17:03:40 +0700

    khuyen mai

--- admin/app/appv3.1.4/controllers/voucher/index_sale.php
+++ admin/app/appv3.1.4/controllers/voucher/index_sale.php
@@ -2,143 +2,43 @@
 
 $data = [];
 
-$list_sale = $this->User_model->get_list_user_working(1, implode(",", [SALE]));
-$list_khach = $this->User_model->get_list_user_working(1, implode(",", [CUSTOMER]));
+$cur_uid = $this->_session_uid();
+
+// lấy danh sách ID voucher mà user đó đc gán
+$list_voucher = $this->Voucher_model->voucher_user_get_list('', $cur_uid);
+
+// lấy thông tin voucher từ ID trên
+if (!empty($list_voucher)) {
+
+    $arr_id = [];
+    foreach ($list_voucher as $voucher) {
+        $arr_id[] = $voucher['id_voucher'];
+    }
+
+    $id_voucher    = implode(',', $arr_id);;
+    $f_price       = '';     // 
+    $t_price       = '';
+    $price_unit    = '';     // 1 phần trăm;2 VND; 3 Đô la; 3 ...
+    $code          = '';
+    $f_expire      = '';     // lọc theo ngày hết hạn
+    $t_expire      = '';     // lọc theo ngày hết hạn
+    $status        = '';     // lọc theo trạng thái
+    $f_create_time = '';
+    $t_create_time = '';
+    $note          = '';
+    $create_by     = '';
+    $limit         = 10000;
+    $offset        = 0;
+
+    $list =  $this->Voucher_model->get_list2($id_voucher, $f_price, $t_price, $price_unit, $code, $f_expire, $t_expire, $status, $f_create_time, $t_create_time, $note, $create_by, $limit, $offset);
+}
+
+$data['list'] = $list;
 
 $header = [
     'title' => 'Quản lý mã giảm giá',
     'header_page_css_js' => 'voucher'
 ];
-
-// SUBMIT FORM (nếu có)
-if (isset($_POST['action'])) {
-    $code               = removeAllTags($this->input->post('code'));
-    $note               = removeAllTags($this->input->post('note'));
-    $voucher_user_sale  = $this->input->post('voucher_user_sale[]');
-    $voucher_user_khach = $this->input->post('voucher_user_khach[]');
-    $price              = removeAllTags($this->input->post('price'));
-    $price_unit         = removeAllTags($this->input->post('price_unit'));
-    $limit              = removeAllTags($this->input->post('limit'));
-    $expire_date        = removeAllTags($this->input->post('expire_date'));
-    $status             = removeAllTags($this->input->post('status'));
-    $id_voucher         = removeAllTags($this->input->post('id_voucher'));
-    $id_voucher         = is_numeric($id_voucher) && $id_voucher > 0 ? $id_voucher : 0;
-
-    $create_time        = date('Y-m-d H:i:s');
-
-    // VALIDATE DATA
-
-    $code        = $code                   != '' ? $code : 0;
-    $note        = $note                   != '' ? $note : 0;
-    $price       = is_numeric($price) || $price > 0 ? $price : 0;
-    $price_unit  = in_array($price_unit, [1, 2, 3, 4]) ? $price_unit : 0;
-    $limit       = is_numeric($limit) && $limit > 0 ? $limit : 0;
-    $expire_date = strtotime($expire_date) == false ? 0 : $expire_date;
-    $status      = $status                 == 'on' ? 0 : 1;
-
-    // dữ liệu không hợp lệ => báo lỗi
-    if (!$code || !$note || !$price || !$price_unit || !$limit || !$expire_date) {
-        $this->session->set_flashdata('flsh_msg', 'Dữ liệu không hợp lệ!');
-        redirect('voucher');
-    }
-
-    // tài khoản được gán mã không tồn tại => báo lỗi
-    $voucher_user_sale  = $voucher_user_sale  == null ? [] : $voucher_user_sale;
-    $voucher_user_khach = $voucher_user_khach == null ? [] : $voucher_user_khach;
-    $all_sale_khach = $list_sale + $list_khach;
-    $voucher_user_new = array_merge($voucher_user_khach, $voucher_user_sale);
-
-    foreach ($voucher_user_new as $id_user) {
-        if (isset($all_sale_khach[$id_user]) == false) {
-            $this->session->set_flashdata('flsh_msg', 'Tài khoản được cấp mã không tồn tại!');
-            redirect('voucher');
-        }
-    }
-
-    // giảm giá theo phần trăm lớn hơn 100% => báo lỗi
-    if ($price_unit == 1 && $price > 100) {
-        $this->session->set_flashdata('flsh_msg', 'Giảm giá không được quá 100%!');
-        redirect('voucher');
-    }
-
-    //END VALIDATE
-
-    // TẠO MỚI 
-    if ($_POST['action'] == 'add') {
-
-        // update voucher
-        $newid = $this->Voucher_model->add($code, $note, $price, $price_unit, $status, $limit, $expire_date, $create_time);
-
-        // thêm user voucher
-
-        if (count($voucher_user_new)) {
-            $exc = $this->Voucher_model->add_multiple_voucher_user($voucher_user_new, $newid, $create_time);
-        }
-
-        //error
-        $this->session->set_flashdata('flsh_msg', $exc ? 'OK' : 'Lưu không thành công vui lòng thử lại!');
-        redirect('voucher');
-    }
-
-    // CẬP NHẬT
-    if ($_POST['action'] == 'edit') {
-
-        $info =  $this->Voucher_model->get_info($id_voucher);
-        if (empty($info)) {
-            $msg = 'Lưu không thành công vui lòng thử lại!';
-        } else {
-
-            // update voucher
-            $exc = $this->Voucher_model->edit($code, $note, $price, $price_unit, $status, $limit, $expire_date, $id_voucher);
-
-            // thêm xóa user khỏi voucher
-            $voucher_user_new = array_merge($voucher_user_khach, $voucher_user_sale);
-            $voucher_user_old = [];
-            foreach ($info['voucher_user'] as $user) {
-                $voucher_user_old[] = $user['id_user'];
-            }
-            $new = array_diff($voucher_user_new, $voucher_user_old);
-            $del = array_diff($voucher_user_old, $voucher_user_new);
-
-            if (count($new)) {
-                $exc = $this->Voucher_model->add_multiple_voucher_user($new, $id_voucher, $create_time);
-            }
-
-            if (count($del)) {
-                $exc = $this->Voucher_model->delete_multiple_voucher_user($del, $id_voucher);
-            }
-
-            //error
-            $this->session->set_flashdata('flsh_msg', $exc ? 'OK' : 'Lưu không thành công vui lòng thử lại!');
-            redirect('voucher');
-        }
-        $this->session->set_flashdata('flsh_msg', $msg);
-        redirect('voucher');
-    }
-}
-
-$id_voucher    = '';
-$f_price       = '';     // 
-$t_price       = '';
-$price_unit    = '';     // 1 phần trăm;2 VND; 3 Đô la; 3 ...
-$code          = '';
-$f_expire      = '';     // lọc theo ngày hết hạn
-$t_expire      = '';     // lọc theo ngày hết hạn
-$status        = '';     // lọc theo trạng thái
-$f_create_time = '';
-$t_create_time = '';
-$note          = '';
-$create_by     = '';
-$limit         = 10000;
-$offset        = 0;
-
-$list =  $this->Voucher_model->get_list2($id_voucher, $f_price, $t_price, $price_unit, $code, $f_expire, $t_expire, $status, $f_create_time, $t_create_time, $note, $create_by, $limit, $offset);
-
-$data['list'] = $list;
-$data['list_sale'] = $list_sale;
-$data['list_khach'] = $list_khach;
-
-
 $this->_loadHeader($header);
 $this->load->view($this->_template_f . 'voucher/index_sale_view', $data);
 $this->_loadFooter();
--- admin/app/appv3.1.4/views/v2023/voucher/index_sale_view.php
+++ admin/app/appv3.1.4/views/v2023/voucher/index_sale_view.php
@@ -21,93 +21,65 @@
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <div class="d-flex justify-content-between align-items-center">
-                                <h3 class="card-title">Danh sách khuyến mãi</h3>
-                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-voucher" data-type="add">
-                                    Thêm mới
-                                </button>
-                            </div>
+            <table id="example1" class="table table-bordered table-striped">
+                <thead class="thead-danger">
+                    <tr>
+                        <th class="text-center">STT</th>
+                        <th style="min-width: 200px; width: 200px;">CODE</th>
+                        <th class="">Mô tả</th>
+                        <th class="text-right">Giảm giá</th>
+                        <th class="text-center">Hết hạn</th>
+                        <th class="text-center">Giới hạn</th>
+                        <th class="text-center">Đã dùng</th>
+                        <th class="text-center" style="min-width: 80px; width: 80px;">Trạng thái</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $index = 1; ?>
+                    <?php foreach ($list as $id_voucher => $item) { ?>
 
-                        </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <table id="example1" class="table table-bordered table-striped">
-                                <thead class="thead-danger">
-                                    <tr>
-                                        <th class="text-center">STT</th>
-                                        <th style="min-width: 200px; width: 200px;">CODE</th>
-                                        <th class="">Mô tả</th>
-                                        <th class="text-right">Giảm giá</th>
-                                        <th class="text-center">Hết hạn</th>
-                                        <th class="text-center">Giới hạn</th>
-                                        <th class="text-center">Đã dùng</th>
-                                        <th class="text-center" style="min-width: 80px; width: 80px;">Trạng thái</th>
-                                        <th style="min-width: 70px; width: 70px;">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php $index = 1; ?>
-                                    <?php foreach ($list as $id_voucher => $item) { ?>
+                        <?php $da_het_han = strtotime($item['expire_date']) < time(); ?>
+                        <tr <?= $da_het_han ? 'style="text-decoration-line: line-through"' : '' ?>>
+                            <td class="align-middle text-center"><?= $index++ ?></td>
+                            <td class="align-middle"><?= $item['code'] ?></td>
+                            <td class="align-middle"><?= $item['note'] ?></td>
+                            <td class="align-middle text-right">
+                                <?= voucher_value($item['price'], $item['price_unit']); ?>
+                            </td>
 
-                                        <?php $da_het_han = strtotime($item['expire_date']) < time(); ?>
-                                        <tr <?= $da_het_han ? 'style="text-decoration-line: line-through"' : '' ?>>
-                                            <td class="align-middle text-center"><?= $index++ ?></td>
-                                            <td class="align-middle"><?= $item['code'] ?></td>
-                                            <td class="align-middle"><?= $item['note'] ?></td>
-                                            <td class="align-middle text-right">
-                                                <?= voucher_value($item['price'], $item['price_unit']); ?>
-                                            </td>
+                            <td class="align-middle text-center">
+                                <?= date('d/m/Y H:i:s ', strtotime($item['expire_date'])) ?>
+                            </td>
 
-                                            <td class="align-middle text-center">
-                                                <?= date('d/m/Y H:i:s ', strtotime($item['expire_date'])) ?>
-                                            </td>
+                            <td class="align-middle text-center">
+                                <?= $item['limit'] ?> lần
+                            </td>
 
-                                            <td class="align-middle text-center">
-                                                <?= $item['limit'] ?> lần
-                                            </td>
+                            <td class="align-middle text-center">
+                                <?php if (count($item['voucher_order'])) { ?>
+                                    <?= count($item['voucher_order']) ?> đơn
+                                    <a href="#" data-toggle="modal" data-target="#modal-voucher-order" data-price='<?= $item['price'] ?>' data-price-unit='<?= $item['price_unit'] ?>' data-order="<?= htmlentities(json_encode($item['voucher_order'])) ?>">
+                                        [VIEW]
+                                    </a>
+                                <?php } else {
+                                    echo '0 đơn';
+                                } ?>
 
-                                            <td class="align-middle text-center">
-                                                <?php if (count($item['voucher_order'])) { ?>
-                                                    <?= count($item['voucher_order']) ?> đơn
-                                                    <a href="#" data-toggle="modal" data-target="#modal-voucher-order" data-price='<?= $item['price'] ?>' data-price-unit='<?= $item['price_unit'] ?>' data-order="<?= htmlentities(json_encode($item['voucher_order'])) ?>">
-                                                        [VIEW]
-                                                    </a>
-                                                <?php } else {
-                                                    echo '0 đơn';
-                                                } ?>
+                            </td>
 
-                                            </td>
-
-                                            <td class="align-middle text-center">
-                                                <?php
-                                                if ($item['status'] === '1') {
-                                                    echo '<span class="badge bg-danger">OFF</span>';
-                                                } else {
-                                                    echo '<span class="badge bg-success">ON</span>';
-                                                }
-                                                ?>
-                                            </td>
-                                            <td class="align-middle text-center">
-                                                <a href="#" data-toggle="modal" data-target="#modal-voucher" data-count_order="<?= count($item['voucher_order']) ?>" data-het_han='<?= $da_het_han ? '1' : '0' ?>' data-type="edit" data-voucher="<?= htmlentities(json_encode($item)) ?>">
-                                                    [EDIT]
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    <?php } ?>
-                                </tbody>
-                            </table>
-                        </div>
-                        <!-- /.card-body -->
-                    </div>
-                    <!-- /.card -->
-                </div>
-                <!-- /.col -->
-            </div>
-            <!-- /.row -->
+                            <td class="align-middle text-center">
+                                <?php
+                                if ($item['status'] === '1') {
+                                    echo '<span class="badge bg-danger">OFF</span>';
+                                } else {
+                                    echo '<span class="badge bg-success">ON</span>';
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
         </div>
         <!-- /.container-fluid -->
     </section>
@@ -115,7 +87,6 @@
 </div>
 
 <!-- load modal add edit -->
-<?php $this->load->view('v2023/voucher/model_add_edit_view'); ?>
 
 <?php $this->load->view('v2023/voucher/model_voucher_order_view'); ?>
 
