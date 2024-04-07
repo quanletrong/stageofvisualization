59fa9327cfb12542c851d94980077b72c364433f
quanletrong <quanletrong@admicro.vn>
2024-04-07 21:59:55 +0700

    cd time

--- admin/app/appv3.1.4/views/v2023/component/header/header_common_view.php
+++ admin/app/appv3.1.4/views/v2023/component/header/header_common_view.php
@@ -42,7 +42,7 @@
 
 
 <!-- common js -->
-<script src="js/v2023/common.js?v=3"></script>
+<script src="js/v2023/common.js?v=4"></script>
 <script src="js/v2023/func.js"></script>
 
 <script src="https://cdn.socket.io/4.7.2/socket.io.min.js"></script>
