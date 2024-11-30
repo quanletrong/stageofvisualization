e9f7db39916b8eea16e729a552ebb09f9058a788
quanletrong <quanletrong@admicro.vn>
2024-12-01 02:12:44 +0700

    cronjob bak

--- admin/app/appv3.1.4/models/backup/Backup_model.php
+++ admin/app/appv3.1.4/models/backup/Backup_model.php
@@ -49,7 +49,7 @@ class Backup_model extends CI_Model
         $datatime = date('Y-m-d H:s:i');
 
         $placeholders = implode(',', array_fill(0, count($list_id), '?'));
-        $sql = "UPDATE tbl_bak_order SET bak_date_time = $datatime WHERE id_discuss IN ($placeholders)";
+        $sql = "UPDATE tbl_bak_order SET bak_date_time = '$datatime' WHERE id_discuss IN ($placeholders)";
 
         $stmt = $iconn->prepare($sql);
         if ($stmt) {
